75c21498d66d014e3e7beac23093dc85c32ae791
Sport <Sport@email.com>
2022-05-09 20:56:57 -0500

    Updated do_outputs function, along with updated get_inputs to comply with do_outputs.

--- hilo/game/card.py
+++ hilo/game/card.py
@@ -20,12 +20,22 @@ class Card:
         self.second_card_value = 0
     
     def draw_first_card(self):
+        """Draws first card value.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
         # Adil Rafi (remove pass once finished)
 
         self.first_card_value = random.randint(1, 13)
 
 
     def draw_second_card(self):
+        """Draws second card value, different from first card value.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
         # Oleksii Zaloznyi (remove pass once finished)
 
         pass
--- hilo/game/director.py
+++ hilo/game/director.py
@@ -17,6 +17,10 @@ class Director:
         Args:
             self (Director): an instance of Director.
         """
+        self.first_card = Card.draw_first_card()
+        self.second_card = Card.draw_second_card()
+        self.draw_card = None
+        self.player_card = None
         self.is_playing = True
         self.score = 0
         self.final_score = 0
@@ -38,13 +42,19 @@ class Director:
         Args:
             self (Director): An instance of Director.
         """
-        draw_card = input("Draw card? Y/N: ")
-        self.is_playing = draw_card == "y"
+        print(f"First Card: {self.first_card}")
+
+        while self.player_card.lower() != "h" or self.player_card.lower() != "l":
+            self.player_card = input("Draw higher card or lower card? (h/l): ")
+        else:
+            print("Choose h for higher, or l for lower.")
 
         # Adil Rafi (remove pass once finished)
 
     def do_updates(self):
         """Updates the player's score.
+        The player earns 100 points if they guessed correctly.
+        The player loses 75 points if they guessed incorrectly.
 
         Args:
             self (Director): An instance of Director.
@@ -60,6 +70,20 @@ class Director:
         Args:
             self (Director): An instance of Director.
         """
-        # Stephen Port
+        print(f"Next Card: {self.second_card}")
 
-        pass
+        if self.final_score == 0:
+            print(self.final_score)
+            self.is_playing = False
+        else:
+            print(self.score)
+            self.draw_card = input("Draw card? (y/n): ")
+            if self.draw_card.lower() == "y":
+                self.is_playing = True
+                self.player_card = None
+            elif self.draw_card.lower() == "n":
+                self.is_playing = False
+            else:
+                print("Choose y for yes, or n for no")
+
+        # Stephen Port
